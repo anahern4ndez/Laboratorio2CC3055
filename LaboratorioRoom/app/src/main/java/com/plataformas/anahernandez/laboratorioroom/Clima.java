12a68ca726fb75b9110f89353cfb3a2e77f98685
her17138 <her17138@uvg.edu.gt>
2018-05-09 21:15:33 -0600

    start de networking (volley)

---
+++ LaboratorioRoom/app/src/main/java/com/plataformas/anahernandez/laboratorioroom/Clima.java
@@ -0,0 +1,84 @@
+package com.plataformas.anahernandez.laboratorioroom;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase que describe los elementos que ira guardando Room
+ * Created by anahernandez on 5/9/18.
+ */
+
+public class Clima implements Parcelable {
+
+    private String containerName;
+    private ArrayList<String> elementosAnidados;
+
+    public Clima(ArrayList<String> elementosAnidados, String containerName) {
+        this.containerName = containerName;
+        this.elementosAnidados = elementosAnidados;
+    }
+
+    @Override
+    public String toString() {
+        String hilo = "\""+ containerName + "\"; ";
+
+        for (String el: elementosAnidados)
+        {
+            hilo += el +", ";
+        }
+        return hilo;
+    }
+    public ArrayList<String> getAnidados()
+    {
+        return elementosAnidados;
+    }
+
+    public String getNombre()
+    {
+        return containerName;
+    }
+
+    public boolean containsElemento(String texto)
+    {
+        boolean contiene = false;
+        for (String el : elementosAnidados)
+        {
+            if (el.contains(texto))
+            {
+                contiene = true;
+            }
+        }
+        return contiene;
+    }
+
+    protected Clima(Parcel in) {
+        elementosAnidados = in.readArrayList(String.class.getClassLoader());
+        containerName = in.readString();
+    }
+
+    public static final Creator<Clima> CREATOR = new Creator<Clima>() {
+        @Override
+        public Clima createFromParcel(Parcel in) {
+            return new Clima(in);
+        }
+
+        @Override
+        public Clima[] newArray(int size) {
+            return new Clima[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringList(elementosAnidados);
+        dest.writeString(containerName);
+    }
+}
--- LaboratorioRoom/app/src/main/java/com/plataformas/anahernandez/laboratorioroom/MainActivity.java
+++ LaboratorioRoom/app/src/main/java/com/plataformas/anahernandez/laboratorioroom/MainActivity.java
@@ -1,7 +1,12 @@
 package com.plataformas.anahernandez.laboratorioroom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,29 +18,33 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     String urlWeather = "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22";
     JSONObject resultWeather;
-    JSONObject objRequest;
+    List<Clima> listaweather = new ArrayList<>(); //lista que contiene todos los elementos del url
+    List<Clima> display = new ArrayList<>(); //lista que contiene los elementos que se mostraran en el listview
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RequestQueue queue  = Volley.newRequestQueue(this);
-
         JsonObjectRequest objRequest = new JsonObjectRequest(Request.Method.GET, urlWeather, null, new Response.Listener<JSONObject>()
         {
                     @Override
                     public void onResponse(JSONObject response) {
-                        try {
-                            resultWeather = (JSONObject) response.getJSONArray("weather").get(0);
-                            System.out.println(resultWeather.getString("description"));
-                            System.out.println(response.getJSONObject("main").getString("temp"));
-                        }catch (JSONException e){
-                            e.printStackTrace();
+                        listaweather = fillInfo(response);
+                        for (Clima el : listaweather)
+                        {
+                            if (el.getNombre().contains("main"))
+                            {
+                                display.add(el);
+                            }
                         }
-
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -45,5 +54,101 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         queue.add(objRequest);
+
+        ArrayList<String> prueba = new ArrayList<>();
+        prueba.add("fdsjak");
+        display.add(new Clima(prueba, "hola"));
+
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<Clima> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,display);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                        //Abre la activity 2, mandando como objeto el hospital
+                        Intent test = new Intent(view.getContext(), ActivityB.class);
+                        startActivity(test);
+
+                    }
+                }
+        );
+    }
+
+    public List<Clima> fillInfo(JSONObject response)
+    {
+
+        List<Clima> toAdd = new ArrayList<>();
+
+        try {
+            ArrayList<String> elementos = new ArrayList<>();
+
+            resultWeather = (JSONObject) response.getJSONArray("weather").get(0);
+            elementos.add("id: " + resultWeather.getString("id"));
+            elementos.add("main: " + resultWeather.getString("main"));
+            elementos.add("icon: " + resultWeather.getString("icon"));
+            elementos.add("description: " + resultWeather.getString("description"));
+            Clima dato = new Clima(elementos, "weather");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add(response.getString("base"));
+            dato = new Clima(elementos, "base");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add("temp: " + response.getJSONObject("main").getString("temp"));
+            elementos.add("pressure: " + response.getJSONObject("main").getString("pressure"));
+            elementos.add("humidity: " + response.getJSONObject("main").getString("humidity"));
+            elementos.add("temp_min: " + response.getJSONObject("main").getString("temp_min"));
+            elementos.add("temp_max: " + response.getJSONObject("main").getString("temp_max"));
+            dato = new Clima(elementos, "main");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add(response.getString("visibility"));
+            dato = new Clima(elementos, "visibility");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add("speed: " + response.getJSONObject("wind").getString("speed"));
+            elementos.add("deg: " + response.getJSONObject("wind").getString("deg"));
+            dato = new Clima(elementos, "wind");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add("all: " + response.getJSONObject("clouds").getString("all"));
+            dato = new Clima(elementos, "clouds");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add(response.getString("dt"));
+            dato = new Clima(elementos, "dt");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add("type: " + response.getJSONObject("sys").getString("type"));
+            elementos.add("id: " + response.getJSONObject("sys").getString("id"));
+            elementos.add("message: " + response.getJSONObject("sys").getString("message"));
+            elementos.add("country: " + response.getJSONObject("sys").getString("country"));
+            elementos.add("sunrise: " + response.getJSONObject("sys").getString("sunrise"));
+            elementos.add("sunset: " + response.getJSONObject("sys").getString("sunset"));
+            dato = new Clima(elementos, "sys");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add(response.getString("id"));
+            dato = new Clima(elementos, "id");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add(response.getString("name"));
+            dato = new Clima(elementos, "name");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+            elementos.add(response.getString("cod"));
+            dato = new Clima(elementos, "cod");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
+
+
+            System.out.println(response.getJSONObject("main").getString("temp"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return toAdd;
     }
 }
