e3ce8e7acc0319d09c09d7e3f44c27325b67bb20
her17138 <her17138@uvg.edu.gt>
2018-05-17 08:09:01 -0600

    bug fixes
    add de activity view de la segunda actividad (la que recibe los datos)
    
    traslado de información de MainActivity hacia ActivityB

--- LaboratorioRoom/app/src/main/java/com/plataformas/anahernandez/laboratorioroom/ActivityB.java
+++ LaboratorioRoom/app/src/main/java/com/plataformas/anahernandez/laboratorioroom/ActivityB.java
@@ -2,12 +2,41 @@ package com.plataformas.anahernandez.laboratorioroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityB extends AppCompatActivity {
+
+    BaseDatos climaDatabase;
+    List<Clima> lweather = new ArrayList<>();
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+
+        climaDatabase = BaseDatos.getDatabase(this);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lweather = climaDatabase.dao().getAll();
+                startView();
+            }
+        }).start();
+
+
+    }
+    public void startView()
+    {
+        TextView tv = findViewById(R.id.tituloDB);
+        tv.setText("Información de la base de datos: ");
+        listView = (ListView) findViewById(R.id.listView2);
+        ArrayAdapter<Clima> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,lweather);
+        listView.setAdapter(adapter);
     }
 }
--- LaboratorioRoom/app/src/main/java/com/plataformas/anahernandez/laboratorioroom/MainActivity.java
+++ LaboratorioRoom/app/src/main/java/com/plataformas/anahernandez/laboratorioroom/MainActivity.java
@@ -1,5 +1,6 @@
 package com.plataformas.anahernandez.laboratorioroom;
 
+import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     String urlWeather = "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22";
     JSONObject resultWeather;
     List<Clima> listaweather = new ArrayList<>(); //lista que contiene todos los elementos del url
-    final List<Clima> display = new ArrayList<>(); //lista que contiene los elementos que se mostraran en el listview
+    List<String> display = new ArrayList<>(); //lista que contiene los elementos que se mostraran en el listview
     ListView listView;
     BaseDatos climaDatabase;
 
@@ -33,53 +35,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RequestQueue queue  = Volley.newRequestQueue(this);
+
         climaDatabase = BaseDatos.getDatabase(this);
 
+        final RequestQueue queue  = Volley.newRequestQueue(this);
         JsonObjectRequest objRequest = new JsonObjectRequest(Request.Method.GET, urlWeather, null, new Response.Listener<JSONObject>()
         {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // List view
-                        listView = (ListView) findViewById(R.id.listView);
-
-                        // Nuevo intent, el de antes no funcionaba :(
-                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                Intent intent = new Intent(MainActivity.this, ActivityB.class);
-                                startActivity(intent);
-                            }
-                        });
-
-                        // Informacion a mostrar
-                        List<String> data = new ArrayList<>();
-                        // Adapter de la lista
-                        final ArrayAdapter<String> adapt = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data);
-                        listView.setAdapter(adapt);
-                        // Load data
-                        listaweather = fillInfo(response);
-                        for(Clima cl: listaweather){
-                            data.add(cl.toString());
+            @Override
+            public void onResponse(JSONObject response) {
+                listaweather = fillInfo(response);
+                //para que haga el display en el listview de unicamente la informacion de main
+                for (Clima el : listaweather)
+                {
+                    if (el.getNombre().equals("main"))
+                    {
+                        for (String anidado : el.getAnidados()) {
+                            display.add(anidado);
                         }
-                        // Notify data changed
-                        adapt.notifyDataSetChanged();
-                        new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                climaDatabase.dao().nukeTable(); //esto para que no se agregue la misma informacion al DB cada vez que corra el programa
-                                climaDatabase.dao().insertAll(listaweather);
-                            }
-                        }).start();
                     }
-                }, new Response.ErrorListener() {
+                }
+                loadListView();
+                new Thread(new Runnable() {
                     @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println(error.getMessage());
+                    public void run() {
+                        climaDatabase.dao().nukeTable(); //esto para que no se agregue la misma informacion al DB cada vez que corra el programa
+                        climaDatabase.dao().insertAll(listaweather);
                     }
-                });
+                }).start();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.getMessage());
+            }
+        });
         queue.add(objRequest);
 
+    }
+    public void loadListView()
+    {
+        TextView tv = findViewById(R.id.tituloLista);
+        tv.setText("Información de \"main\": ");
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,display);
+        listView.setAdapter(adapter);
+        System.out.println(display);
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        //Abre la activity 2
+                        Intent test = new Intent(view.getContext(), ActivityB.class);
+                        startActivity(test);
+
+                    }
+                }
+        );
     }
 
     public List<Clima> fillInfo(JSONObject response)
@@ -90,16 +102,28 @@ public class MainActivity extends AppCompatActivity {
         try {
             ArrayList<String> elementos = new ArrayList<>();
 
+            elementos = new ArrayList<>();
+            elementos.add("lon: " + response.getJSONObject("coord").getString("lon"));
+            elementos.add("lat: " + response.getJSONObject("coord").getString("lat"));
+            Clima dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("coord");
+            toAdd.add(dato);
+            elementos = new ArrayList<>();
             resultWeather = (JSONObject) response.getJSONArray("weather").get(0);
             elementos.add("id: " + resultWeather.getString("id"));
             elementos.add("main: " + resultWeather.getString("main"));
             elementos.add("icon: " + resultWeather.getString("icon"));
             elementos.add("description: " + resultWeather.getString("description"));
-            Clima dato = new Clima(elementos, "weather");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("weather");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add(response.getString("base"));
-            dato = new Clima(elementos, "base");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("base");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add("temp: " + response.getJSONObject("main").getString("temp"));
@@ -107,24 +131,34 @@ public class MainActivity extends AppCompatActivity {
             elementos.add("humidity: " + response.getJSONObject("main").getString("humidity"));
             elementos.add("temp_min: " + response.getJSONObject("main").getString("temp_min"));
             elementos.add("temp_max: " + response.getJSONObject("main").getString("temp_max"));
-            dato = new Clima(elementos, "main");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("main");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add(response.getString("visibility"));
-            dato = new Clima(elementos, "visibility");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("visibility");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add("speed: " + response.getJSONObject("wind").getString("speed"));
             elementos.add("deg: " + response.getJSONObject("wind").getString("deg"));
-            dato = new Clima(elementos, "wind");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("wind");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add("all: " + response.getJSONObject("clouds").getString("all"));
-            dato = new Clima(elementos, "clouds");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("clouds");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add(response.getString("dt"));
-            dato = new Clima(elementos, "dt");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("dt");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add("type: " + response.getJSONObject("sys").getString("type"));
@@ -133,19 +167,27 @@ public class MainActivity extends AppCompatActivity {
             elementos.add("country: " + response.getJSONObject("sys").getString("country"));
             elementos.add("sunrise: " + response.getJSONObject("sys").getString("sunrise"));
             elementos.add("sunset: " + response.getJSONObject("sys").getString("sunset"));
-            dato = new Clima(elementos, "sys");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("sys");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add(response.getString("id"));
-            dato = new Clima(elementos, "id");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("id");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add(response.getString("name"));
-            dato = new Clima(elementos, "name");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("name");
             toAdd.add(dato);
             elementos = new ArrayList<>();
             elementos.add(response.getString("cod"));
-            dato = new Clima(elementos, "cod");
+            dato = new Clima();
+            dato.setAnidados(elementos);
+            dato.setNombre("cod");
             toAdd.add(dato);
 
 
